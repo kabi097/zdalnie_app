a2e10feeb43defc9584baee5f195c7cf4d1c7163
kabi097 <paweljanwiczynski@gmail.com>
2019-08-17 12:12:55 +0200

    AdPage improved

---
+++ src/components/RepliesList.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card>
+        dsds
+    </v-card>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="css">
+    
+</style>
--- src/views/AdPage.vue
+++ src/views/AdPage.vue
@@ -20,22 +20,23 @@
                 </v-toolbar-items>
               </v-toolbar>
               <v-divider></v-divider>
+              <div class="ad_properties"></div>
               <v-list dense>
                 <v-list-item>
                   <v-icon class="pr-4">mdi-xbox-controller</v-icon>
                   <div class="title">Zlecę przebudowę strony internetowej</div>
                 </v-list-item>
                 <v-list-item>
-                  <v-icon class="pr-4">mdi-cash</v-icon>
-                  Budżet: 400 zł
-                </v-list-item>
-                <v-list-item>
-                  <v-icon class="pr-4">mdi-calendar-range</v-icon>
-                  Pozostało: 4 dni
-                </v-list-item>
-                <v-list-item>
-                  <v-icon class="pr-4">mdi-account-group</v-icon>
-                  Liczba zgłoszeń: 14
+                  <ul style="list-style: none; padding: 0">
+                  <li><v-icon class="pr-4">mdi-cash</v-icon>
+                  Budżet: 400 zł</li>
+
+                  <li><v-icon class="pr-4">mdi-calendar-range</v-icon>
+                  Pozostało: 4 dni</li>
+
+                  <li><v-icon class="pr-4">mdi-account-group</v-icon>
+                  Liczba zgłoszeń: 14</li>
+                  </ul>
                 </v-list-item>
                 <v-list-item>
                   <v-list-item-content>
@@ -45,20 +46,25 @@
                 </v-list-item>
               </v-list>
               <p class="mx-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eius libero ab, assumenda atque unde illo ipsa odit, rem ducimus ut repudiandae minima, quo ad repellendus eum fugiat doloribus voluptatem?</p>
-              
-
             </v-card>
+            <replies-list></replies-list>
           </v-flex>
       </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {
+import RepliesList from '@/components/RepliesList.vue';
 
+export default {
+  components: {
+    RepliesList
+  }
 }
 </script>
 
 <style>
-
+  .ad_properties {
+    
+  }
 </style>
