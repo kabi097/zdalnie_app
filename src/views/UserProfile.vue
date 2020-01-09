3d1dca9a840efaf2c7594ed741eca6b0f5caaac5
kabi097 <paweljanwiczynski@gmail.com>
2020-01-09 11:37:56 +0100

    UserProfile changed

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,35 +1,34 @@
 <template>
-    <v-container>
-        <v-row no-gutters>
-            <v-col cols="3">
-                <profile-card />
-            </v-col>
-            <v-col cols="9">
-                <v-card>
+    <div>
+        <div class="header">
+
+        </div>
+        <v-container>
+            <v-card>
+            <v-row no-gutters>
+                <v-col cols="3">
+                    <profile-card />
+                </v-col>
+                <v-col cols="9">
                     <v-tabs v-model="tab">
                         <v-tab>Profil</v-tab>
                         <v-tab>Ogłoszenia</v-tab>
                         <v-tab>Odpowiedzi</v-tab>
                         <v-tab>Ustawienia</v-tab>
                     </v-tabs>   
-                    <v-tabs-items v-model="tab">
-                        <v-tab-item>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, enim esse animi modi nobis assumenda quidem nesciunt quibusdam minus a atque nostrum optio repudiandae repellat cum voluptatum explicabo ullam sapiente.
-                        </v-tab-item>
-                        <v-tab-item>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, enim esse animi modi nobis assumenda quidem nesciunt quibusdam minus a atque nostrum optio repudiandae repellat cum voluptatum explicabo ullam sapiente.
-                        </v-tab-item>
-                        <v-tab-item>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, enim esse animi modi nobis assumenda quidem nesciunt quibusdam minus a atque nostrum optio repudiandae repellat cum voluptatum explicabo ullam sapiente.
-                        </v-tab-item>
-                        <v-tab-item>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, enim esse animi modi nobis assumenda quidem nesciunt quibusdam minus a atque nostrum optio repudiandae repellat cum voluptatum explicabo ullam sapiente.
-                        </v-tab-item>
-                    </v-tabs-items>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+                    <div class="px-2 mb-3">
+                        <div class="body-2 py-2 font-weight-medium">Opis</div>
+                        <div class="font-weight-regular pb-2">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt corporis consequuntur sint, error dolor id saepe. Nisi itaque modi aliquam nesciunt sed, ipsam hic voluptas, quibusdam soluta cupiditate commodi natus? 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga illo voluptates saepe, est laudantium autem nisi molestias magni facere, explicabo libero cumque officiis minima quas nostrum inventore velit a eius!
+                            lorem</p>
+                        </div>
+                    </div>
+                </v-col>
+            </v-row>
+            </v-card>
+        </v-container>
+    </div>
 </template>
 <script>
 import ProfileCard from '@/components/ProfileCard.vue';
