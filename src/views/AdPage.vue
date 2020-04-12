498f81d4174cf39d6837fbaf5935f0e5d100adf5
kabi097 <paweljanwiczynski@gmail.com>
2020-04-13 00:29:20 +0200

    Fixed loading post data

--- src/views/AdPage.vue
+++ src/views/AdPage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-      <v-row no-gutters>
+      <v-row v-if="post" no-gutters>
           <v-col cols="6" md="3" order="0" order-md="0"> 
             <v-btn @click="$router.back()" text class="my-2 text-none text-left font-weight-regular">
               <v-icon left>mdi-arrow-left</v-icon>
